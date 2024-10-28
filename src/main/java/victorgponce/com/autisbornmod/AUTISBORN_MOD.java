906d10f36e2d8df5008fce4f2c805a585a7d5f85
ponchisao326_ <ponchisao326@hotmail.com>
2024-10-28 01:34:03 +0100

    Changed LOGGER from private to public for using it on other classes

--- src/main/java/victorgponce/com/autisbornmod/AUTISBORN_MOD.java
+++ src/main/java/victorgponce/com/autisbornmod/AUTISBORN_MOD.java
@@ -33,7 +33,7 @@ public class AUTISBORN_MOD {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "autisborn_mod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "autisborn_mod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "autisborn_mod" namespace
