258a37ea83c8e9c9632e0130e9743c3a050f8673
ponchisao326_ <ponchisao326@hotmail.com>
2024-10-28 13:17:06 +0100

    Optimized imports and removed unused code

--- src/main/java/victorgponce/com/autisbornmod/AUTISBORN_MOD.java
+++ src/main/java/victorgponce/com/autisbornmod/AUTISBORN_MOD.java
@@ -5,8 +5,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,7 +16,6 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
