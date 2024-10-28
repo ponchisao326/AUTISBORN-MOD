b089210289beb8f98193e8dc5c4fedcb0d6d0b12
ponchisao326_ <ponchisao326@hotmail.com>
2024-10-28 12:59:04 +0100

    added comments

--- src/main/java/victorgponce/com/autisbornmod/Screens/CustomMainScreen.java
+++ src/main/java/victorgponce/com/autisbornmod/Screens/CustomMainScreen.java
@@ -28,22 +28,24 @@ import static net.minecraft.network.Connection.connectToServer;
 public class CustomMainScreen extends Screen {
 
     private Minecraft minecraftInstance = Minecraft.getInstance();
-    private static final int TITLE_Y_POS = 30; // Y position of the title on screen
+    private static final int TITLE_Y_POS = 30;
     private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation("victorgponce:autisbornmod/textures/background.png");
     private static final ResourceLocation TITLE_TEXTURE = new ResourceLocation("victorgponce:autisbornmod/textures/title.png");
-    private String status = "El servidor se encuentra OFFLINE"; // Server status
-    private final ExecutorService executorService = Executors.newFixedThreadPool(3); // Thread pool for multiple threads
+    private String status = "El servidor se encuentra OFFLINE";
+    private final ExecutorService executorService = Executors.newFixedThreadPool(3);
     private static final String SERVER_ADDRESS = "node-marb.ponchisaohosting.xyz";
     private static final int SERVER_PORT = 25566;
     private static final Component COPYRIGHT_TEXT = Component.literal("Menu creado por PonchisaoHosting");
 
     public CustomMainScreen() {
         super(Component.literal("Custom Main Menu"));
+        // Iniciamos el SubProceso que hace ping al servidor
         startPingThread();
     }
 
     @Override
     protected void init() {
+        // Seteamos variables de los botones
         this.minecraftInstance = Minecraft.getInstance();
         int buttonWidth = 200;
         int buttonHeight = 20;
@@ -51,41 +53,35 @@ public class CustomMainScreen extends Screen {
         int copyrightWidth = this.font.width(COPYRIGHT_TEXT);
         int j = this.width - copyrightWidth - 2;
 
-        // Button "AUTISBORN" - Direct connection to the server
         this.addRenderableWidget(new Button(this.width / 2 - 100, l + 24, buttonWidth, buttonHeight,
                 Component.literal("AUTISBORN"), button -> {
             this.minecraftInstance.execute(() -> {
-                // Crea un objeto ServerData con la dirección y el puerto del servidor
+                // Conexión directa al servidor al pulsar el botón
                 ServerData serverData = new ServerData("AUTISBORN", SERVER_ADDRESS + ":" + SERVER_PORT, false);
-                // Llama al método join para conectarse al servidor
                 join(serverData);
             });
         }));
 
-
-        // Button "Opciones" - Opens options menu
         this.addRenderableWidget(new Button(this.width / 2 - 100, l + 68, 98, 20,
                 Component.translatable("menu.options"), button -> executorService.submit(() ->
                 this.minecraftInstance.setScreen(new OptionsScreen(this, this.minecraftInstance.options)))));
 
-        // Button "Quit Game" - Exit the game
         this.addRenderableWidget(new Button(this.width / 2 + 2, l + 68, 98, 20,
                 Component.translatable("menu.quit"), button -> executorService.submit(this.minecraftInstance::stop)));
 
-        // Button "MODS" - Opens the Forge mods menu
         this.addRenderableWidget(new Button(this.width / 2 - 100, l + 23 * 2, buttonWidth, buttonHeight,
                 Component.translatable("fml.menu.mods"), button -> executorService.submit(() -> {
             assert this.minecraftInstance != null;
             this.minecraftInstance.setScreen(new net.minecraftforge.client.gui.ModListScreen(this));
         })));
 
-        // Button for opening URL
+        // Botón de Copyright del Menu
         this.addRenderableWidget(new PlainTextButton(j - 3, this.height - 20, copyrightWidth, 10, COPYRIGHT_TEXT, (button) -> {
             executorService.submit(() -> {
                 try {
                     Util.getPlatform().openUrl(new URL("https://victorgponce.com"));
                 } catch (IOException e) {
-                    e.printStackTrace(); // Handle the exception, e.g., log it
+                    e.printStackTrace();
                 }
             });
         }, this.font));
@@ -93,26 +89,25 @@ public class CustomMainScreen extends Screen {
 
     @Override
     public void render(@NotNull PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+        // Habilidamos las texturas y el sistema de render
         RenderSystem.enableTexture();
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 
-        // Draw background
+        // Seteamos el fondo
         RenderSystem.setShaderTexture(0, BACKGROUND_TEXTURE);
         blit(poseStack, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
 
-        // Draw custom title
+        // Seteamos el titulo
         RenderSystem.setShaderTexture(0, TITLE_TEXTURE);
         int titleWidth = (int) Math.round(928 / 3.5);
         int titleHeight = (int) Math.round(253 / 3.5);
-        int titleX = (this.width - titleWidth) / 2; // Center title
+        int titleX = (this.width - titleWidth) / 2;
 
-        // Draw title without repeating the texture
         blit(poseStack, titleX, TITLE_Y_POS, 0, 0, titleWidth, titleHeight, titleWidth, titleHeight);
 
-        // Render server status
+        // Seteamos el Status con el subproceso de Ping iniciado anteriormente
         drawString(poseStack, this.font, status, 10, this.height - 20, 0xFFFFFF);
 
-        // Render buttons and other elements
         super.render(poseStack, mouseX, mouseY, partialTicks);
     }
 
@@ -121,15 +116,16 @@ public class CustomMainScreen extends Screen {
             while (!Thread.currentThread().isInterrupted()) {
                 status = pingServer(SERVER_ADDRESS, SERVER_PORT) ? "El servidor se encuentra ONLINE" : "El servidor se encuentra OFFLINE";
                 try {
-                    Thread.sleep(4000); // Check every 4 seconds
+                    Thread.sleep(4000);
                 } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt(); // Restore interrupt status
-                    break; // Exit loop if interrupted
+                    Thread.currentThread().interrupt();
+                    break;
                 }
             }
         });
     }
 
+    // Pingeamos al server
     private boolean pingServer(String address, int port) {
         try (Socket socket = new Socket()) {
             socket.connect(new InetSocketAddress(address, port), 1000);
@@ -143,21 +139,10 @@ public class CustomMainScreen extends Screen {
         ConnectScreen.startConnecting(this, this.minecraft, ServerAddress.parseString(p_99703_.ip), p_99703_);
     }
 
-
-    // public void connectToServer(String ip, int port) {
-    //     ServerData serverData = new ServerData("Servidor", ip + ":" + port, false);
-    //     assert Minecraft.getInstance().screen != null;
-    //     ConnectScreen.startConnecting(
-    //             Minecraft.getInstance().screen,
-    //             Minecraft.getInstance(),
-    //             ServerAddress.parseString(serverData.ip),
-    //             serverData
-    //     );
-    // }
-
+    // Al cerrar la pantalla detenemos el subproceso pues no se usa y cerramos la pantalla
     @Override
     public void onClose() {
-        executorService.shutdownNow(); // Stop the ping thread when closing the screen
+        executorService.shutdownNow();
         super.onClose();
     }
 }
