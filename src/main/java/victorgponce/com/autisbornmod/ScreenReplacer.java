d476cca4cdad5af923356a3d58d441443f45d690
ponchisao326_ <ponchisao326@hotmail.com>
2024-10-28 02:07:52 +0100

    Removed unused imports

--- src/main/java/victorgponce/com/autisbornmod/ScreenReplacer.java
+++ src/main/java/victorgponce/com/autisbornmod/ScreenReplacer.java
@@ -1,10 +1,9 @@
 package victorgponce.com.autisbornmod;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraft.client.Minecraft;
