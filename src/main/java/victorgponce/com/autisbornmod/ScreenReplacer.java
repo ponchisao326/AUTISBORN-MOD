27708e211eb3582b4003b65c4f0f76dd391635d7
ponchisao326_ <ponchisao326@hotmail.com>
2024-10-28 01:33:09 +0100

    Changed Window Title and forced to init on fullscreen to avoid problems

--- src/main/java/victorgponce/com/autisbornmod/ScreenReplacer.java
+++ src/main/java/victorgponce/com/autisbornmod/ScreenReplacer.java
@@ -1,5 +1,6 @@
 package victorgponce.com.autisbornmod;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -8,14 +9,18 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
+import com.mojang.blaze3d.platform.Window;
 import victorgponce.com.autisbornmod.Screens.CustomMainScreen;
 
 @EventBusSubscriber(modid = AUTISBORN_MOD.MODID, bus = Bus.FORGE, value = Dist.CLIENT)
 public class ScreenReplacer {
-
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event) {
         Minecraft mc = Minecraft.getInstance();
+        Window mcWindow = mc.getWindow();
+
+        mcWindow.setTitle("AUTISBORN - Powered by PonchisaoHosting");
+        mcWindow.changeFullscreenVideoMode();
 
         if (event.phase == TickEvent.Phase.START) {
             // Verifica si la pantalla actual es la pantalla de título
